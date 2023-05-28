60df913845896c1a686c1eee5638a3740201d33a
Oleg <olegolegovich@inbox.ru>
2023-05-28 12:57:55 +0300

    HW6_done

--- HW_06_01.py
+++ HW_06_01.py
@@ -4,3 +4,8 @@
 an = a1 + (n-1) * d.
 Каждое число вводится с новой строки.'''
 
+a1 = int(input('Введите a1: '))
+d = int(input('Введите d: '))
+n = int(input('Введите n: '))
+for i in range(n):
+    print(a1 + i * d)
